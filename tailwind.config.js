69728bfa28d191b868456e6eb16f7019e0649604
johnhilt95 <hilton9505@gmail.com>
2023-06-26 15:40:59 +0530

    Front-end Assignment using tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-pattern":
+          "url('/images/viktor-forgacs-XBGrm3qPCTw-unsplash 1.png')",
+      },
+    },
+  },
+  plugins: [],
+};
